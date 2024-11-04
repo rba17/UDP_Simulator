4ad21f3675f8f464e52c134618205ca1e9dbb16c
ryan <rba17@mail.aub.edu>
2024-11-04 21:19:47 +0200

    leaving jaffet

--- Client_A.java
+++ Client_A.java
@@ -6,15 +6,15 @@ public class Client_A {
     public static void main(String[] args) throws IOException {
 
         final int packetNumber = 1000;
+        
+        // A recieving 
 
         DatagramSocket ds = new DatagramSocket();
         InetAddress ip = InetAddress.getLocalHost();
 
-        byte[] buf = new byte[1024];
-        
         for (int i = 0; i < packetNumber; i++) {
             String message = "hi";
-            buf = message.getBytes();
+            byte[] buf = message.getBytes();
             DatagramPacket dps = new DatagramPacket(buf, buf.length, ip, Client_B.portB);
             ds.send(dps);
         }
--- Client_B.java
+++ Client_B.java
@@ -2,9 +2,27 @@ import java.io.IOException;
 import java.net.*;
 
 public class Client_B {
-    static final int portB =  9999;
+    static final int portB = 9999;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        final int packetNumber = 1000;
+
+        // B recieving
+
+        DatagramSocket ds = new DatagramSocket(portB);
+        InetAddress ip = InetAddress.getLocalHost();
+
+        byte[] buf = new byte[1024];
+
+        for (int i = 0; i < packetNumber; i++) {
+            DatagramPacket dpr = new DatagramPacket(buf, buf.length);
+            ds.receive(dpr);
+            String message = new String(dpr.getData(), 0, dpr.getLength());
+            System.out.println(message + " " + (i + 1));
+        }
+
+        // B sending
 
     }
 }
