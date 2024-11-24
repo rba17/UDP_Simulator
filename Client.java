b9afc9fc20ea6a0ffb11ef2a916428c29a27c2f8
ryan <rba17@mail.aub.edu>
2024-11-24 22:54:42 +0200

    100% and some bonuses

---
+++ Client.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Utils.resetFile();
+
+        InetAddress ip = InetAddress.getLocalHost();
+
+        DatagramSocket socket = new DatagramSocket();
+        socket.setSoTimeout(1000);
+
+        Thread com_A2B = new Thread(
+                new Communication(socket, ip, Utils.channelPort, 'A', 'B'));
+        Thread com_C2F = new Thread(
+                new Communication(socket, ip, Utils.channelPort, 'C', 'F'));
+
+        com_A2B.start();
+        com_C2F.start();
+
+        com_A2B.join();
+        com_C2F.join();
+
+        socket.close();
+    }
+
+    static class Communication implements Runnable {
+        private DatagramSocket ds;
+        private InetAddress ip;
+        private int port;
+        private char sender;
+        private char receiver;
+
+        public Communication(DatagramSocket ds, InetAddress ip, int port, char sender, char receiver) {
+            this.ds = ds;
+            this.ip = ip;
+            this.port = port;
+            this.sender = sender;
+            this.receiver = receiver;
+        }
+
+        public void run() {
+            try {
+                long sendStartTime = System.nanoTime();
+                String fullMessage = "";
+                byte[] buf;
+                Random rand = new Random();
+                Utils.printSendTime(sender, receiver, sendStartTime, System.nanoTime());
+
+                for (int i = 0; i < Utils.packetNumber; i++) {
+                    
+                    // send
+                    fullMessage = Utils.generateRandomMessage(rand.nextInt(6) + 5) + "$" + i + sender + receiver;
+                    // messages between 5 and 10 characters long ^
+                    buf = fullMessage.getBytes();
+                    // buffers of varied sizes ^
+                    DatagramPacket dps = new DatagramPacket(buf, buf.length, ip, port);
+                    // packets of varied sizes ^
+                    ds.send(dps);
+
+                    // recieve
+                    // we put try catch block incase unrelaible channel didnt send (lost) and
+                    // timer timed out after 1000ms
+
+                    try {
+
+                        byte[] rcv = new byte[Utils.BUFFER_SIZE];
+                        DatagramPacket dpr = new DatagramPacket(rcv, rcv.length);
+                        ds.receive(dpr);
+                        String receivedMessage = new String(dpr.getData(), 0, dpr.getLength()).trim();
+                        System.out.println(receivedMessage);
+
+                    } catch (SocketTimeoutException e) {
+                        System.out.println("NACK(" + i + ")");
+                    }
+                }
+
+                long sendEndTime = System.nanoTime();
+                Utils.printReceiveTime(sender, receiver, sendStartTime, sendEndTime);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Utils.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Utils {
+
+    final static double lossFactor = 0.1;
+    final static int packetNumber = 100;
+    final static int channelPort = 9999;
+    final static int BUFFER_SIZE = 1024;
+    public static File f = new File("Report.txt");
+
+    public static void printSendTime(char sender, char receiver, long t0, long t1) {
+        System.out.println("\nSending packets from " + sender + " to " + receiver + " at elapsed:\t"
+                + (t1 - t0) / 1_000_000_000.0 + "s\n");
+    }
+
+    public static void printReceiveTime(char sender, char receiver, long t0, long t1) {
+        System.out.println("\n" + receiver + " received packets from " + sender + " at elapsed:\t"
+                + (t1 - t0) / 1_000_000_000.0 + "s\n");
+    }
+
+    public static synchronized void resetFile() throws IOException {
+        try (FileWriter out = new FileWriter(f, false)) {
+            out.write("");
+        }
+    }
+
+    public static synchronized void writeToFile(String s) throws IOException {
+        try (FileWriter out = new FileWriter(f, true)) {
+            out.append(s);
+        }
+    }
+
+    public static void printFile(File f) throws FileNotFoundException {
+        try (Scanner read = new Scanner(f)) {
+            while (read.hasNextLine())
+                System.out.println(read.nextLine());
+        }
+    }
+
+    public static String generateRandomMessage(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random rand = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(rand.nextInt(characters.length())));
+        }
+        return sb.toString();
+    }
+
+}
